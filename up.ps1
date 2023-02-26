486d86491fa3982bbabc523660cdf50b2dbc3582
Logan Harber <chalharb@gmail.com>
2023-02-26 16:13:08 -0600

    chore: Created docker functionality

---
+++ up.ps1
@@ -0,0 +1,17 @@
+param (
+    [string]$environment = "development"
+)
+
+Write-Host "====================================="
+if ($environment -eq "production") {
+    $composeFile = ".docker\docker-compose.prod.yml"
+    Write-Host "INFO: RUNNING PRODUCTION MODE"
+}
+else {
+    $composeFile = ".docker\docker-compose.yml"
+    Write-Host "INFO: RUNNING DEVELOP MODE"
+}
+
+Write-Host "INFO: Using docker-compose file: $composeFile"
+Write-Host "====================================="
+docker compose -f $composeFile up -d --build
