f392999f60c34e3c489a29810de8b34bbd3e4a31
Logan Harber <chalharb@gmail.com>
2025-03-16 15:26:39 -0500

    feat: add metadata title and description

--- app/constants.ts
+++ app/constants.ts
@@ -23,9 +23,7 @@ export const JOB_HISTORY: TimelineEntry[] = [
       startDate: new Date("2024-05-01"),
       endDate: new Date(),
     },
-    description: `
-      Shaping overall Sitecore roadmap and I'm currently leading an effort to modernize the Sitecore platform with the latest technologies.
-    `,
+    description: `I'm currently shaping the overall Sitecore roadmap and leading an effort to completely modernize the Sitecore platform using the latest technologies.`,
   },
   {
     jobTitle: "Consulting Application Engineer",
@@ -48,9 +46,7 @@ export const JOB_HISTORY: TimelineEntry[] = [
       startDate: new Date("2021-07-01"),
       endDate: new Date("2024-05-01"),
     },
-    description: `
-      I collaborated with stakeholders to shape product direction, led the Engineering Community to define standards, and managed unit testing efforts. I oversaw GitHub Enterprise configuration, conducted code reviews, mentored junior engineers, and contributed to hiring. I refined requirements, developed CI/CD pipelines, created technical documentation, and developed a Sitecore application using React, .NET, Azure DevOps, and GitHub Actions.
-    `,
+    description: `I collaborated with stakeholders to shape product direction, led the Engineering Community to define standards, and managed unit testing efforts. I oversaw GitHub Enterprise configuration, conducted code reviews, mentored junior engineers, and contributed to hiring. I refined requirements, developed CI/CD pipelines, created technical documentation, and developed a Sitecore application using React, .NET, Azure DevOps, and GitHub Actions.`,
   },
   {
     jobTitle: "Senior Application Engineer",
@@ -74,9 +70,7 @@ export const JOB_HISTORY: TimelineEntry[] = [
       startDate: new Date("2019-11-01"),
       endDate: new Date("2021-07-01"),
     },
-    description: `
-      I developed a Coronavirus Assessment Tool with Microsoft Health Bot, managed Ataccama applications for data management, and streamlined development with Gulp.js and Webpack.js. I implemented CI/CD with Azure DevOps, conducted code reviews, mentored team members, and created documentation for DotCMS.
-    `,
+    description: `Developed a Coronavirus Assessment Tool with Microsoft Health Bot, managed Ataccama applications for data management, and streamlined development with Gulp.js and Webpack.js. I implemented CI/CD with Azure DevOps, conducted code reviews, mentored team members, and created documentation for DotCMS.`,
   },
   {
     jobTitle: "Front End Application Engineer II",
@@ -90,9 +84,7 @@ export const JOB_HISTORY: TimelineEntry[] = [
       startDate: new Date("2016-12-01"),
       endDate: new Date("2019-11-01"),
     },
-    description: `
-      I led GitHub Enterprise migrations, developed and maintained core website functionality, implemented responsive design, and collaborated with teams for front-end integration. I automated testing, optimized performance, enhanced security, created Python automations, and ensured web accessibility compliance.
-    `,
+    description: `Led GitHub Enterprise migrations, developed and maintained core website functionality, implemented responsive design, and collaborated with teams for front-end integration. I automated testing, optimized performance, enhanced security, created Python automations, and ensured web accessibility compliance.`,
   },
   {
     jobTitle: "UX/UI Engineer",
@@ -106,9 +98,7 @@ export const JOB_HISTORY: TimelineEntry[] = [
       startDate: new Date("2016-09-01"),
       endDate: new Date("2016-12-01"),
     },
-    description: `
-      I collaborated with stakeholders to create design prototypes, translated mockups into functional prototypes using HTML, CSS, JavaScript, and Jekyll, conducted user tests to ensure usability, and audited websites for ADA compliance.
-    `,
+    description: `Collaborated with stakeholders to create design prototypes, translated mockups into functional prototypes using HTML, CSS, JavaScript, and Jekyll, conducted user tests to ensure usability, and audited websites for ADA compliance.`,
   },
   {
     jobTitle: "Full Stack Engineer",
@@ -122,8 +112,6 @@ export const JOB_HISTORY: TimelineEntry[] = [
       startDate: new Date("2015-04-01"),
       endDate: new Date("2016-09-01"),
     },
-    description: `
-      I collaborated with clients to create over 80 bespoke websites and applications, focusing on SEO to improve visibility and user experience. I developed pixel-perfect solutions with designers and managed networking and infrastructure for over 200 websites, ensuring reliable performance and security.
-    `,
+    description: `Collaborated with clients to create over 80 bespoke websites and applications, focusing on SEO to improve visibility and user experience. I developed pixel-perfect solutions with designers and managed networking and infrastructure for over 200 websites, ensuring reliable performance and security.`,
   },
 ];
